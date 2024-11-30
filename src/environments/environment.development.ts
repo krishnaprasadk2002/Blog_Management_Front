823f4a9ca28b77c129ab69a07f95af5738020757
Krishnaprasadk2002 <prasadkrishna1189@gmail.com>
2024-11-30 10:32:34 +0530

    env changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
+    // production: false,
+    // apiUrl: 'http://localhost:3000'
     production: false,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'http://api.blogmanagement.kpkz.online'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
+    // production: true,
+    // apiUrl: 'http://localhost:3000'
     production: true,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'http://api.blogmanagement.kpkz.online'
   };
